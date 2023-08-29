fd2e18149d91426c77634f38d52bcc09228e2f49
MarkJLee <MarkJLee@users.noreply.github.com>
2023-08-29 23:09:34 +0900

    feat: handle various main image inputs

--- lib/features/home/widgets/post_media_widget.dart
+++ lib/features/home/widgets/post_media_widget.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PostMediaWidget extends StatelessWidget {
-  final Widget image;
+  final List<Widget>? images;
   const PostMediaWidget({
-    super.key,
-    required this.image,
-  });
+    Key? key,
+    this.images,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class PostMediaWidget extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8),
           child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.30,
+            height: MediaQuery.of(context).size.height * 0,
             child: const VerticalDivider(
               color: Colors.grey,
               thickness: 1,
@@ -27,9 +27,28 @@ class PostMediaWidget extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: image,
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: (images == null || images!.isEmpty)
+                      ? Container()
+                      : images?.length == 1
+                          ? images![0]
+                          : SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.25,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: images!.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: images![index],
+                                  );
+                                },
+                              ),
+                            ),
+                ),
               ),
               const Padding(
                 padding: EdgeInsets.only(top: 16),
