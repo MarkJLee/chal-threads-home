ec6540e1692e289b49c28650b72687bd782d9d3d
MarkJLee <MarkJLee@users.noreply.github.com>
2023-09-02 17:08:15 +0900

    chore: add users_data

---
+++ lib/features/search/widgets/users_data.dart
@@ -0,0 +1,138 @@
+class Users {
+  final String account;
+  final String name;
+  final String userIcon;
+  final int followers;
+  bool follow = false;
+
+  Users({
+    required this.account,
+    required this.name,
+    required this.userIcon,
+    required this.followers,
+    required this.follow,
+  });
+}
+
+List<Users> users_data = [
+  Users(
+      account: 'buddy_the_pup',
+      name: 'Buddy Pup',
+      userIcon: 'assets/s0.jpg',
+      followers: 2361,
+      follow: false),
+  Users(
+      account: 'coco_lover',
+      name: 'Coco Lover',
+      userIcon: 'assets/s1.jpg',
+      followers: 38358,
+      follow: false),
+  Users(
+      account: 'max_adventures',
+      name: 'Max Adventure',
+      userIcon: 'assets/s2.jpg',
+      followers: 471841,
+      follow: false),
+  Users(
+      account: 'lucy_cuddles',
+      name: 'Lucy Cuddle',
+      userIcon: 'assets/s3.jpg',
+      followers: 4495546,
+      follow: false),
+  Users(
+      account: 'charlie_fun',
+      name: 'Charlie Fun',
+      userIcon: 'assets/s4.jpg',
+      followers: 873,
+      follow: false),
+  Users(
+      account: 'milo_play',
+      name: 'Milo Play',
+      userIcon: 'assets/s5.jpg',
+      followers: 84,
+      follow: false),
+  Users(
+      account: 'bailey_walks',
+      name: 'Bailey Walk',
+      userIcon: 'assets/s6.jpg',
+      followers: 51346,
+      follow: false),
+  Users(
+      account: 'bella_tails',
+      name: 'Bella Tail',
+      userIcon: 'assets/s7.jpg',
+      followers: 809158,
+      follow: false),
+  Users(
+      account: 'luna_night',
+      name: 'Luna Night Moon',
+      userIcon: 'assets/s8.jpg',
+      followers: 2212066,
+      follow: false),
+  Users(
+      account: 'bocky_hills',
+      name: 'Rocky Hill Stone',
+      userIcon: 'assets/s9.jpg',
+      followers: 371029,
+      follow: false),
+  Users(
+      account: 'caisy_fields',
+      name: 'Daisy Field',
+      userIcon: 'assets/s10.jpg',
+      followers: 4356029,
+      follow: false),
+  Users(
+      account: 'cuke_royal',
+      name: 'Duke Royal King',
+      userIcon: 'assets/s11.jpg',
+      followers: 4262,
+      follow: false),
+  Users(
+      account: 'badie_hugs',
+      name: 'Sadie Hug',
+      userIcon: 'assets/s12.jpg',
+      followers: 806226,
+      follow: false),
+  Users(
+      account: 'ceus_gods',
+      name: 'Zeus God Thunder',
+      userIcon: 'assets/s13.jpg',
+      followers: 29525,
+      follow: false),
+  Users(
+      account: 'ruby_gem',
+      name: 'Ruby Gem Stone',
+      userIcon: 'assets/s14.jpg',
+      followers: 64175,
+      follow: false),
+  Users(
+      account: 'rosie_blooms',
+      name: 'Rosie Bloom Petal',
+      userIcon: 'assets/s15.jpg',
+      followers: 62,
+      follow: false),
+  Users(
+      account: 'teddy_bear',
+      name: 'Teddy Bear Cub',
+      userIcon: 'assets/s16.jpg',
+      followers: 226235,
+      follow: false),
+  Users(
+      account: 'riley_happy',
+      name: 'Riley Happy Joy',
+      userIcon: 'assets/s17.jpg',
+      followers: 9739762,
+      follow: false),
+  Users(
+      account: 'jack_jumps',
+      name: 'Jack Jump Leap',
+      userIcon: 'assets/s18.jpg',
+      followers: 420131,
+      follow: false),
+  Users(
+      account: 'shadow_mystery',
+      name: 'Shadow Mystery Dark',
+      userIcon: 'assets/s19.jpg',
+      followers: 213209,
+      follow: false),
+];
