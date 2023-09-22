f812f0e0d2430b79ec16d0e31c1829a9c288117f
MarkJLee <MarkJLee@users.noreply.github.com>
2023-09-23 07:17:42 +0900

    refactor: user repo, mv

--- lib/features/profile/repos/user_repo.dart
+++ lib/features/profile/repos/user_repo.dart
@@ -1,16 +1,18 @@
 import 'package:chal_threads_home/features/authentication/repos/authentication_repo.dart';
 import 'package:chal_threads_home/features/profile/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserRepository {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
+  // final FirebaseStorage _storage = FirebaseStorage.instance;
   final AuthenticationRepository _authRepo;
 
   UserRepository(this._authRepo);
+
   String? get uid => _authRepo.user?.uid;
+  String? get email => _authRepo.user?.email;
+  String? get account => _authRepo.user?.email?.split('@').first;
 
   Future<void> saveUserProfile(UserModel user) async {
     var userRef = _db.collection("users").doc(uid);
--- lib/features/profile/view_models/user_view_model.dart
+++ lib/features/profile/view_models/user_view_model.dart
@@ -1,21 +1,20 @@
 import 'dart:async';
 
-import 'package:chal_threads_home/features/authentication/repos/authentication_repo.dart';
 import 'package:chal_threads_home/features/profile/models/user_model.dart';
 import 'package:chal_threads_home/features/profile/repos/user_repo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserViewModel extends AsyncNotifier<UserModel> {
   late final UserRepository _userRepo;
-  late final AuthenticationRepository _authRepo;
+
   @override
   FutureOr<UserModel> build() {
     _userRepo = ref.read(userRepositoryProvider);
-    _authRepo = ref.read(authRepo);
+
     return UserModel(
-      uid: _authRepo.user?.uid ?? "",
-      email: _authRepo.user?.email ?? "",
-      account: _authRepo.user?.email?.split('@').first ?? "",
+      uid: _userRepo.uid ?? "",
+      email: _userRepo.email ?? "",
+      account: _userRepo.account ?? "",
     );
   }
 
