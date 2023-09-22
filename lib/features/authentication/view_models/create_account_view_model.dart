b392ad0f33bdc1643d42bb169772d99c32ef5e6a
MarkJLee <MarkJLee@users.noreply.github.com>
2023-09-22 20:16:44 +0900

    feat: add user account to firestore after signUp

--- lib/features/authentication/view_models/create_account_view_model.dart
+++ lib/features/authentication/view_models/create_account_view_model.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:chal_threads_home/features/authentication/repos/authentication_repo.dart';
 import 'package:chal_threads_home/features/home/views/home_screen.dart';
+import 'package:chal_threads_home/features/profile/models/user_model.dart';
+import 'package:chal_threads_home/features/profile/repos/user_repo.dart';
 import 'package:chal_threads_home/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,21 +11,32 @@ import 'package:go_router/go_router.dart';
 
 class CreateAccountViewModel extends AsyncNotifier<void> {
   late final AuthenticationRepository _authRepo;
+  late final UserRepository _userRepo;
 
   @override
   FutureOr<void> build() {
     _authRepo = ref.read(authRepo);
+    _userRepo = ref.read(userRepositoryProvider);
   }
 
   Future<void> signUp(BuildContext context) async {
     state = const AsyncValue.loading();
     final form = ref.read(signUpForm);
-    state = await AsyncValue.guard(
-      () async => await _authRepo.signUp(
+    state = await AsyncValue.guard(() async {
+      await _authRepo.signUp(
         form["email"],
         form["password"],
-      ),
-    );
+      );
+      final String uid = _authRepo.user?.uid ?? "";
+      final String email = _authRepo.user?.email ?? "";
+      final String account = _authRepo.user?.email?.split('@').first ?? "";
+      final UserModel user = UserModel(
+        uid: uid,
+        email: email,
+        account: account,
+      );
+      await _userRepo.saveUserProfile(user);
+    });
     if (state.hasError) {
       showFirebaseErrorSnack(context, state.error);
     } else {
